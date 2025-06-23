6bcb70fd6ceb4d91a402abec48ff16819764ca6a
steven <stevenodu@gmail.com>
2025-06-23 15:33:26 +0300

    updating region name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 resource "aws_vpc" "my-vpc" {
@@ -24,7 +24,7 @@ resource "aws_subnet" "public-subnet" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-west-1a"
 
   tags = {
     Name = "PublicSubnet"
